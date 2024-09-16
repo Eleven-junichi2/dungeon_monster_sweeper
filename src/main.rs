f831fb5eb4303bccfa56565160ffc8fb36fc227a
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2024-09-16 18:31:55 +0900

    make feature: placing enemy on map

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use std::io;
 
-// fn generate_map() ->  {}
+use rand::{self, Rng};
 
+struct Player {
+    pos: Coordinate,
+    strength: u8,
+}
+
+struct Enemy {
+    pos: Coordinate,
+    strength: u8,
+}
+
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
 
 fn main() {
     let floor_progress: u128 = 0;
@@ -9,14 +23,53 @@ fn main() {
     println!("HighScore: 未実装");
     println!("Press any key to continue");
     let mut input: String = String::new();
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
     println!("--- How to play ---");
     println!("Press \"escape key\" to exit from the game");
     println!("Input \"x y\" of your destination (For example, \"12 2\" means go to (12, 2))");
     println!("Press any key to continue");
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
     // マップ生成
     // 生成したマップを表示
     // 現在の階を表示
     // 操作方法を表示
+    let mut rng = rand::thread_rng();
+    let player = Player {
+        pos: Coordinate {
+            x: rng.gen_range(0..=16),
+            y: rng.gen_range(0..=16),
+        },
+        strength: 2,
+    };
+    let mut enemy_list: Vec<Enemy> = Vec::new();
+    // place enemies
+    for _ in 0..rng.gen_range(0..=255) {
+        enemy_list.push(Enemy {
+            pos: loop {
+                let mut pos = Coordinate {
+                    x: rng.gen_range(0..16),
+                    y: rng.gen_range(0..16),
+                };
+                if pos.x == player.pos.x && pos.y == player.pos.y {
+                    continue;
+                } else {
+                    break pos;
+                };
+            },
+            strength: rng.gen_range(0..=255),
+        });
+    }
+    // [y][x]
+    // for row in map {
+    //     println!("{:?}", row);
+    // }
+    let mut map: [[char; 16]; 16] = [['*'; 16]; 16];
+    for enemy in enemy_list {
+        map[enemy.pos.y][enemy.pos.x] = 'E';
+    };
+    map[player.pos.y][player.pos.x] = 'P';
+    for row in map {
+        println!("{}", &row.iter().collect::<String>());
+    };
+    // visible mapの実装
 }
